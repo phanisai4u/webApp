7c8c6ac203d2708031dd97059b07ca72e1e54b68
Phanisai <phanisai@mobileways.in>
2016-05-02 18:25:04 +0530

    Commit Sample post

--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,10 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+router.post('/',function(rer,res,next){
+            
+    res.json({ message: 'phani! welcome to our post api!' });   
+            
+});
 
 module.exports = router;
